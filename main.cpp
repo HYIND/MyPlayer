f0afabc4eaaef78a11be30ab6de53e260bfa9ca8
HYIND <1255231699@qq.com>
2022-09-08 15:01:37 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+
+extern "C"
+{
+
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libavdevice/avdevice.h"
+#include "libavformat/version.h"
+#include "libavutil/time.h"
+#include "libavutil/mathematics.h"
+
+#undef main
+
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+
+    qDebug()<<"this test ffmpeg!";
+//    av_register_all();
+    unsigned version = avcodec_version();
+    qDebug()<<"ffmpeg version:"<<version;
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
